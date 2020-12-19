bcd5f9c074368cad3f360526e29143f6428246ce
Imran <imranhsayed@gmail.com>
2020-12-19 11:35:52 +0530

    Add the required files

---
+++ footer.php
@@ -0,0 +1,19 @@
+
+<?php
+/**
+ * Footer template
+ *
+ * @package Advanced WooCommerce Theme
+ */
+?>
+
+
+<footer>
+	<h3>Footer</h3>
+
+</footer>
+</div>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Header template.
+ *
+ * @package Advanced WooCommerce Theme
+ */
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+<?php
+if ( function_exists( 'wp_body_open' ) ) {
+	wp_body_open();
+}
+?>
+
+<div id="page" class="site">
+	<header id="masthead" class="site-header" role="banner">
+		Header
+	</header>
+	<div id="content" class="site-content">
